37247323850d74be4368c818e6c0b5c9b17f988f
benbraide <benbraide@yahoo.com>
2022-05-03 17:07:18 +0100

    Implemented 'x-keyboard:type' directive extension

--- src/directive/extended/keyboard.ts
+++ src/directive/extended/keyboard.ts
@@ -1,6 +1,8 @@
+import { FindComponentById } from "../../component/find";
 import { AddDirectiveHandler } from "../../directives/add";
 import { CreateDirectiveHandlerCallback } from "../../directives/callback";
 import { EvaluateLater } from "../../evaluator/evaluate-later";
+import { IComponent } from "../../types/component";
 import { ResolveOptions } from "../options";
 
 function BindKeyboardInside(contextElement: HTMLElement, callback: (isInside: boolean, unbind: () => void) => void){
@@ -28,12 +30,41 @@ function BindKeyboardKey(contextElement: HTMLElement, key: 'down' | 'up', callba
     contextElement.addEventListener(`key${key}`, (e) => callCallback(e.key));
 }
 
-export const KeyboardDirectiveHandler = CreateDirectiveHandlerCallback('keyboard', ({ componentId, contextElement, expression, argKey, argOptions }) => {
+const DefaultKeyboardTypeDelay = 500;
+
+function BindKeyboardType(component: IComponent | null, contextElement: HTMLElement, delay: number, callback: (typing: boolean) => void){
+    let checkpoint = 0, reset = () => {
+        ++checkpoint;
+    };
+
+    component?.FindElementScope(contextElement)?.AddUninitCallback(reset);
+    let lastValue = false, callCallback = (value: boolean) => {
+        if (value != lastValue){
+            callback(lastValue = value);
+        }
+    };
+    
+    let afterDelay = (myCheckpoint: number) => {
+        if (myCheckpoint == checkpoint){
+            callCallback(false);
+        }
+    };
+
+    contextElement.addEventListener('keydown', () => {
+        let myCheckpoint = ++checkpoint;
+        callCallback(true);
+        setTimeout(() => afterDelay(myCheckpoint), ((delay < 0) ? DefaultKeyboardTypeDelay : delay));
+    });
+}
+
+export const KeyboardDirectiveHandler = CreateDirectiveHandlerCallback('keyboard', ({ component, componentId, contextElement, expression, argKey, argOptions }) => {
     let options = ResolveOptions({
         options: {
+            delay: -1,
             once: false,
         },
         list: argOptions,
+        defaultNumber: -1,
     });
     
     let assign = (value: any) => {
@@ -52,6 +83,9 @@ export const KeyboardDirectiveHandler = CreateDirectiveHandlerCallback('keyboard
     }
     else if (argKey === 'down' || argKey === 'up'){
         BindKeyboardKey(contextElement, argKey, assign);
+    }
+    else if (argKey === 'type'){
+        BindKeyboardType((component || FindComponentById(componentId)), contextElement, options.delay, assign);
     }
 });
 
